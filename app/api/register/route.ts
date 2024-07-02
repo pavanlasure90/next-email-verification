aed66cfb41b894ae06513384ad93aff1322f3204
pavanlasure90 <113824942+pavanlasure90@users.noreply.github.com>
2024-07-02 12:38:34 +0530

    changed

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -7,7 +7,7 @@ export async function POST(req:any) {
     try {
         const {name, email, password} =await req.json();
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword =  await bcrypt.hash(password, 10);
         await connectMongoDB();
         await User.create({name, email, password: hashedPassword})
 
@@ -15,4 +15,4 @@ export async function POST(req:any) {
     } catch (error) {
         return NextResponse.json({message: "An error occured while registering User"}, {status: 500})
     }
-}
+}  
