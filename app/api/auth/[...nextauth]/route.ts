42f1815561e127b6f0373fa2bdbaee68a2c6ba6e
pavanlasure90 <113824942+pavanlasure90@users.noreply.github.com>
2024-07-02 12:44:15 +0530

    next verification

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,7 +8,7 @@ export const authOptions = {
   providers: [
     CredentialsProvider({
       name: "credentials",
-      credentials: {},
+      credentials: {} ,
 
       async authorize(credentials) {
         const { email, password } = credentials;
@@ -29,7 +29,7 @@ export const authOptions = {
 
           return user;
         } catch (error) {
-          console.log("Error: ", error);
+          console.log("Error:", error);
         }
       },
     }),
